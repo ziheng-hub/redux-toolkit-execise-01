4693f962d1612e7a192e11c21fea62c6cecb9662
zihenghub <zihenghub@gmail.com>
2024-01-10 20:20:27 +0900

    add feature of Counter using redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Counter from "./features/counter/Counter";
 
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <Counter></Counter>
+  </div>;
 };
 
 export default App;
