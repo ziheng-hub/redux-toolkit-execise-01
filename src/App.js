f80fe333fe4c0e4fc7b5becfcef9073b14184490
zihenghub <zihenghub@gmail.com>
2024-01-13 14:39:40 +0900

    clear some privious code and initiate new application using redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Counter from "./features/counter/Counter";
 
 const App = () => {
-  return <div>
-    <Counter></Counter>
-  </div>;
+  return <div></div>;
 };
 
 export default App;
